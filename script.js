ef9c8b8301629f44f4e5826822dde477f817008c
Dylan Smith <smithd@catlin.edu>
2017-10-06 10:52:34 -0700

    Version 1.0

---
+++ script.js
@@ -0,0 +1,124 @@
+window.$ = window.jQuery = require('jquery');
+var scout = require("scouting")
+
+scout.init('stand')
+
+scout.page('Login',[12]);
+scout.page('Auto',[12]);
+scout.page('Tele',[4,4,4]);
+scout.page('Save',[12]);
+
+scout.login('.cell-login-1');
+
+scout.multipleChoice(
+	'.cell-auto-1',
+    'Cross-Line',
+    [
+    	// Name, bootstrap color class, OPTIONAL: value
+    	['yes', 'success'],
+    	['no', 'danger']
+    ],
+    'cross_auto'
+);
+
+scout.multipleChoice(
+	'.cell-auto-1',
+	'Gear',
+	[
+		['yes', 'success'],
+		['attempt', 'primary'],
+		['no', 'danger']
+	],
+	'gear_auto'
+);
+
+scout.multipleChoice(
+	'.cell-auto-1',
+	'Shoot',
+	[
+		['yes', 'success'],
+		['attempt', 'primary'],
+		['no', 'danger']
+	],
+	'shoot_auto'
+);
+
+scout.multipleChoice(
+	'.cell-tele-2',
+	'High Goal Shooting',
+	[
+		['yes', 'success'],
+		['attempt', 'primary'],
+		['no', 'danger']
+	],
+	'high_shoot'
+);
+
+scout.multipleChoice(
+	'.cell-tele-2',
+	'Low Goal Shooting',
+	[
+		['yes', 'success'],
+		['attempt', 'primary'],
+		['no', 'danger']
+	],
+	'low_shoot'
+);
+
+scout.counter(
+	'.cell-tele-1',
+	'Gears',
+	1,
+	'gears'
+);
+
+scout.multipleChoice(
+	'.cell-tele-1',
+	'Defense',
+	[
+		['strong', 'success'],
+		['decent', 'primary'],
+		['poor', 'warning'],
+		['none', 'danger']
+	],
+	'defense'
+);
+
+scout.multipleChoice(
+	'.cell-tele-2',
+	'Driver Skill',
+	[
+		['strong', 'success'],
+		['decent', 'primary'],
+		['poor', 'danger'],
+	],
+	'driving'
+);
+
+scout.multipleChoice(
+	'.cell-tele-3',
+	'Climb Speed',
+	[
+		['fast', 'success'],
+		['normal', 'primary'],
+		['slow', 'warning'],
+		['no climb', 'danger']
+	],
+	'climb_speed'
+);
+
+scout.multipleChoice(
+	'.cell-tele-3',
+	'Climb Success',
+	[
+		['successful', 'success'],
+		['attempted', 'primary'],
+		['no climb', 'danger']
+	],
+	'climb_success'
+);
+
+scout.done(
+	'.cell-save-1',
+	false
+);
